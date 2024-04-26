10f5c44a013e531daf968e63b527f8febf662dd8
forLoop94 <charlesuwem94@gmail.com>
2024-04-26 09:29:38 +0100

    Change mongoDB connection from localhost to Atlas, create initial file structure for api

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async() => {
   try {
-    await mongoose.connect("mongodb://localhost/sportRecords");
+    await mongoose.connect(process.env.CONNECTION_STRING);
     console.log("MongoDB connected");
   } catch(error) {
     console.log("Database could not connect:", error)
