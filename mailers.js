8f97823d01f56a7d3a1e4ef1e43de8bf374b83b8
forLoop94 <charlesuwem94@gmail.com>
2024-04-26 16:10:22 +0100

    Cerate a verification schema and model, temporarily store verification data in the database, transporter sends mail only when data is stored

--- mailers.js
+++ mailers.js
@@ -1,7 +1,9 @@
 import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 import nodeMailer from "nodemailer";
+import UserVerification from "./models/authModels/verificationModel.js";
 
 const transporter = nodeMailer.createTransport({
   service: "gmail",
@@ -20,7 +22,6 @@ transporter.verify((err, success) => {
   }
 });
 
-
 export const sendVerificationEmail = async ({ _id, email }, res) => {
   const currentUrl = "http://localhost:3000/";
 
@@ -32,8 +33,40 @@ export const sendVerificationEmail = async ({ _id, email }, res) => {
     subject: "Verify Your Email",
     html: `<p>Verify your email to complete the signip and login into your account.</p><p>This link <b>Expires in 10 minutes</b>Click<a href=${
       currentUrl + "api/auth/verify/" + _id + "/" + uniqueString
-    }>here</a></p>`
+    }>here</a></p>`,
   };
 
-  transporter.sendMail(mailOptions);
+  const salt = 10;
+  bcrypt.hash(uniqueString, salt).then((hashedUniqueString) => {
+    UserVerification.create({
+      userId: _id,
+      uniqueString: hashedUniqueString,
+      createdAt: Date.now(),
+      expiresAt: Date.now() + 600000,
+    })
+      .then(() => {
+        transporter
+          .sendMail(mailOptions)
+          .then(() => {
+            res.status(200).json({
+              status: "PENDING",
+              message: "Verification data saved successfully",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.json({
+              status: "Failed",
+              message: "Email verification failed",
+            });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          status: "Failed",
+          message: "Could not save verification data",
+        });
+      });
+  });
 };
