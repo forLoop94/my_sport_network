e489828c769532ff13b74ea89fafa0048faceea9
forLoop94 <charlesuwem94@gmail.com>
2024-04-26 14:08:17 +0100

    Add login action to authController

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../../controllers/auth/authController.js";
+import { login, registerUser } from "../../controllers/auth/authController.js";
 
 const router = Router();
 
 router.post('/register', registerUser);
+router.post('/login', login);
 
 export default router
