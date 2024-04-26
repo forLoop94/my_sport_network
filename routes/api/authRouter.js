ba70a5d343030a0352c8cb0b874f49f7e3a2f77d
forLoop94 <charlesuwem94@gmail.com>
2024-04-26 16:29:37 +0100

    Find verification data and check if it has expired. If it has, delete that User's records. If it hasn't, update isVerified to true and delete verification data

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { login, registerUser } from "../../controllers/auth/authController.js";
+import { verifyUser } from "../../controllers/auth/verificationController.js";
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', login);
+router.get('/verify/:userId/:uniqueString', verifyUser);
 
 export default router
