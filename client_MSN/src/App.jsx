2b00b8c9621de53116f1317ba9209acb2acb07a9
forLoop94 <charlesuwem94@gmail.com>
2024-04-27 22:33:03 +0100

    Add login and sign up routes to App, test with browser

--- client_MSN/src/App.jsx
+++ client_MSN/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
 
-function App() {
-  return (
-    <section>
-      <h1>app</h1>
-      <button className='btn btn-primary'>press</button>
-    </section>
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Route>
   )
-}
+);
 
-export default App
+const App = () => <RouterProvider router={router} />;
+
+export default App;
